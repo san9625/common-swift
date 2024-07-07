dc2e24cd9cf5dd228055b115e5afe858e47c808c
somayoshikawa <san303jiburi@gmail.com>
2024-07-07 14:26:46 +0900

    modify network utils

--- Sources/Utils/NetworkUtils/NetworkUtils.swift
+++ Sources/Utils/NetworkUtils/NetworkUtils.swift
@@ -9,25 +9,43 @@ import Foundation
 import Network
 import AVFoundation
 
-// デバイスがネットワークに接続できている場合、trueを返却する
-// ネットワーク接続が必要な機能を呼び出す前に、当関数でネットワーク接続状態を確認する
-public func isConnectedToNetwork() -> Bool {
-    let monitor = NWPathMonitor()
-    var isConnected = false
-    let semaphore = DispatchSemaphore(value: 0)
-
-    monitor.pathUpdateHandler = { path in
-        if path.status == .satisfied {
-            isConnected = true
-        } else {
-            isConnected = false
-        }
-        semaphore.signal()
-    }
-    let queue = DispatchQueue(label: "NetworkMonitor")
-    monitor.start(queue: queue)
+public class NetworkManager {
     
-    semaphore.wait() // 状態更新を待つ
-    monitor.cancel()
-    return isConnected
+    public static let shared = NetworkManager()
+    private init() {}
+    
+    // 現在のネットワーク状態を確認し、ネットワークに接続していればtrueを、接続していなければfalse返却する
+    public func isConnectedToNetwork() -> Bool {
+ 
+        let monitor = NWPathMonitor()
+        let currentPath = monitor.currentPath
+        
+        if currentPath.status == .satisfied {
+            return true
+        } else {
+            return false
+        }
+       
+    }
+    
+    // ネットワーク接続の状態変化を監視する
+//    public func isConnectedToNetwork(completion: @escaping (Bool) -> Void) {
+//        
+//        var isConnected = false
+//        let semaphore = DispatchSemaphore(value: 0)
+//        
+//        let monitor = NWPathMonitor()
+//        monitor.pathUpdateHandler = { path in
+//            if path.status == .satisfied {
+//                completion(true)
+//            } else {
+//                completion(false)
+//            }
+//            monitor.cancel() // 状態を確認したら監視を停止
+//        }
+//        
+//        let queue = DispatchQueue(label: "NetworkMonitor")
+//        monitor.start(queue: queue)
+//    }
+    
 }
